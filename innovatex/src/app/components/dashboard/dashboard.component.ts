c51f470d833bbdbdeafad386236468c5e4a1ba3b
aaron Francolino <aaron.e.francolino@gmail.com>
2025-05-15 09:49:20 -0300

    dashboard changes

--- innovatex/src/app/components/dashboard/dashboard.component.ts
+++ innovatex/src/app/components/dashboard/dashboard.component.ts
@@ -11,14 +11,45 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent  implements OnInit {
-  ventas: any[] = [];
+   ventas: any[] = [];
+  cargando = false;
+  error = '';
+  totalIngresos = 0;
 
   constructor(private http: HttpClient) {}
+ ngOnInit(): void {
+    this.obtenerVentas();
+  }
 
-  ngOnInit(): void {
-    this.http.get<any[]>(' http://localhost:3000/api/ventas').subscribe({
-      next: (data) => this.ventas = data,
-      error: (err) => console.error('Error al cargar ventas', err)
+   obtenerVentas() {
+    this.cargando = true;
+    this.error = '';
+    
+    this.http.get<any[]>('http://localhost:3000/api/ventas').subscribe({
+      next: (ventas) => {
+        this.ventas = ventas;
+        this.calcularTotalIngresos();
+        this.cargando = false;
+      },
+      error: (err) => {
+        console.error('Error al obtener ventas:', err);
+        this.error = 'Error al cargar las ventas';
+        this.cargando = false;
+      }
     });
   }
-} 
+   calcularTotalIngresos() {
+    this.totalIngresos = this.ventas.reduce((total, venta) => total + venta.monto, 0);
+  }
+
+  // Función para formatear la fecha
+  formatearFecha(fecha: string): string {
+    return new Date(fecha).toLocaleDateString('es-AR');
+  }
+
+  // Función para formatear el monto
+  formatearMonto(monto: number): string {
+    return `$${monto.toFixed(2)} ARS`;
+  }
+}
+
